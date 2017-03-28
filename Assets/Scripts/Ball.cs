98501aa1953bf95715faec1462ea59f94e8ea2d0
Skye <CharlesBDunnIII@gmail.com>
2017-03-28 13:07:20 -0400

    Added new physics material, ball now bounces according to distance from center of Paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -21,17 +21,26 @@ public class Ball : MonoBehaviour {
 
     void OnCollisionEnter2D(Collision2D otherObject)
     {
+        Debug.Log("OncollisionEnter2D");
         if(paddleName == otherObject.gameObject.tag)
         {
-            padd = otherObject.GetComponent<Paddle>();
+            Debug.Log("Enter IF");
+            padd = otherObject.collider.gameObject.GetComponent<Paddle>();
             paddlePos = otherObject.gameObject.transform.position;
-            
+            ContactPoint2D contact = otherObject.contacts[0];
+            rB2D.AddForce(CalcPosDiff(contact.point, paddlePos));
         }
     }
 
-    private float CalcPosDiff(Vector2 firstPos, Vector2 secondPos)
+    private Vector2 CalcPosDiff(Vector2 impactPoint, Vector2 centerPoint)
     {
         //Clamp return value to max width of paddle and return the difference of the two vecters.
-        throw new System.NotImplementedException();
+        Vector2 totalDist = impactPoint - centerPoint;
+        totalDist.x *= 100;
+        Debug.Log("Dist between ball impact (" + impactPoint + ") and paddlePos (" + centerPoint);
+        totalDist.y = 0.0f;
+        //totalDist.x = Mathf.Clamp(totalDist.x, 0.0f, 0.5f);
+
+        return totalDist;
     }
 }
