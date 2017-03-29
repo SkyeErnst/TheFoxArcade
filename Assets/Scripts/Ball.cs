c313eff35b8295241f17ee76418906cf0fd55f9b
Skye <CharlesBDunnIII@gmail.com>
2017-03-29 10:49:40 -0400

    Getting closer to good ball movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,13 +13,34 @@ public class Ball : MonoBehaviour {
     /// <summary>
     /// The maximum velocity that the ball is allowed to travel
     /// </summary>
-    private readonly float maxVelocity = 30.0f;
+    private readonly float maxVelocity = 35.0f;
 
     /// <summary>
     /// The minimum velocity that the ball is allowed to travel
     /// </summary>
     private readonly float minVelocity = 5.0f;
+
+    /// <summary>
+    /// How much velocity to add to the rigidbody after a bounce
+    /// </summary>
+    private readonly float velocityBounceMultiplier = 30.0f;
+
+    /// <summary>
+    /// How much to multiply the x value of the 
+    /// distance between the ball impact point and the center
+    /// of the paddle
+    /// </summary>
+    private readonly float bounceOffsetMultiplier = 100.0f;
+
+    /// <summary>
+    /// The current position of the paddle
+    /// </summary>
     private Vector2 paddlePos;
+
+    /// <summary>
+    /// Reference to the rigidbody 2d attatched to the same object
+    /// this class is attached to
+    /// </summary>
     private Rigidbody2D rB2D;
     #endregion
 
@@ -35,19 +56,46 @@ public class Ball : MonoBehaviour {
             paddlePos = otherObject.gameObject.transform.position;
             ContactPoint2D contact = otherObject.contacts[0];
             rB2D.AddForce(CalcPosDiff(contact.point, paddlePos));
+            UpdateVelocity();
         }
     }
 
-    void FixedUpadte()
+    void OnCollisionExit2D(Collision2D otherObject)
     {
-        Debug.Log("FixedUpdate");
+        rB2D.AddForce(rB2D.velocity * velocityBounceMultiplier);
+    }
+
+    void FixedUpdate()
+    {
+
+        Debug.Log("Velocity: " + rB2D.velocity);
 
         UpdateVelocity();
     }
 
+    /// <summary>
+    /// Updates the velocity of the ball to ensure it stays within acceptable bounds
+    /// </summary>
     private void UpdateVelocity()
     {
-
+        if(rB2D.velocity.sqrMagnitude > maxVelocity)
+        {
+            if (rB2D.velocity.sqrMagnitude > maxVelocity + 40.0f)
+            {
+                Debug.Log("TOO DAMN FAST");
+                rB2D.velocity = Vector2.ClampMagnitude(rB2D.velocity, maxVelocity);
+            }
+            else
+            {
+                Debug.Log("Decreasing Speed");
+                rB2D.AddForce(-rB2D.velocity * maxVelocity, ForceMode2D.Force);
+            }
+        }
+        if(rB2D.velocity.sqrMagnitude < minVelocity)
+        {
+            Debug.Log("Increasing Speed");
+            rB2D.AddForce(rB2D.velocity * minVelocity, ForceMode2D.Force);
+        }
     }
 
     /// <summary>
@@ -58,12 +106,9 @@ public class Ball : MonoBehaviour {
     /// <returns></returns>
     private Vector2 CalcPosDiff(Vector2 impactPoint, Vector2 centerPoint)
     {
-        //Clamp return value to max width of paddle and return the difference of the two vecters.
         Vector2 totalDist = impactPoint - centerPoint;
-        totalDist.x *= 100;
-        Debug.Log("Dist between ball impact (" + impactPoint + ") and paddlePos (" + centerPoint);
+        totalDist.x *= bounceOffsetMultiplier;
         totalDist.y = 0.0f;
-
         return totalDist;
     }
 }
