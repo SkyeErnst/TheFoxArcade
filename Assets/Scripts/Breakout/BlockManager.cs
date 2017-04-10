9911c48e929d74c15caea6b98ee75ea641cc2ac2
Skye <CharlesBDunnIII@gmail.com>
2017-04-10 13:01:24 -0400

    Added work toward #8
    Renamed all blocks to be sequential numbers to facilitate having each block be in a list so that they can have individual properties.

---
+++ Assets/Scripts/Breakout/BlockManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    #region Private Fields
+    private List<Block> blockList;
+    #endregion
+
+    public void Awake()
+    {
+        blockList = new List<Block>();
+    }
+}
