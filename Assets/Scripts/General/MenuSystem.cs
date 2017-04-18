89b2282d70a02a19568ac1bfdf51332648d9dd08
Skye <CharlesBDunnIII@gmail.com>
2017-04-17 23:54:16 -0400

    More work toward menu system

--- Assets/Scripts/General/MenuSystem.cs
+++ Assets/Scripts/General/MenuSystem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuSystem : MonoBehaviour
@@ -20,6 +21,12 @@ public class MenuSystem : MonoBehaviour
         Paused = 0,
         Unpaused = 1
     }
+    public enum Canvases
+    {
+        BreakoutGame = 0,
+        BreakoutLose = 1,
+        EscMenu = 2
+    }
     #endregion
 
     #region Public Properties
@@ -27,14 +34,48 @@ public class MenuSystem : MonoBehaviour
     /// Current state of being -Globaly- paused or unpaused
     /// </summary>
     public GlobalPauseState PauseState { get; set; }
-    #endregion 
+    #endregion
+
+    #region Public Fields
+    /// <summary>
+    /// List of all canvases
+    /// </summary>
+    public List<Canvas> CanvasList;
+
+    public Text BreakoutResultDisplayText;
+
+    public Dictionary<Canvases, string> CanvasDict;
+    #endregion
 
     #region Private Fields
     /// <summary>
-    /// Text to display when the player clears the level
+    /// The canvas tag as stored in the tag manager
     /// </summary>
-    private readonly string winText = "Cleared! \n Nice job!";
+    private const string CANVAS_TAG = "Canvas";
+
+    /// <summary>
+    /// The text to be displayed when a breakout level is WON
+    /// </summary>
+    private const string BREKOUT_WIN_TEXT = "Cleared! \n Nice job!";
+
+    /// <summary>
+    /// The text to be displayed when a breakout level is LOST
+    /// </summary>
+    private const string BREKOUT_LOSE_TEXT = "Nice try...";
     #endregion
 
+    //try again / cleared text goes here
 
+    private void Awake()
+    {
+        GameObject[] goArray = GameObject.FindGameObjectsWithTag(CANVAS_TAG);
+        CanvasList = new List<Canvas>();
+        CanvasDict = new Dictionary<Canvases, string>();
+
+        foreach (GameObject canvasGO in goArray)
+        {
+            CanvasList.Add(canvasGO.GetComponent<Canvas>());
+        }
+        
+    }
 }
